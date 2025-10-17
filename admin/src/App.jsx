32a033cd332e251af197d08e6997d22c07a38271
Hemanjali Saride <125305300+saridehemanjali@users.noreply.github.com>
2025-10-17 20:56:15 +0530

    Fix import case for Navbar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/sidebar/sidebar'
 import {Routes,Route} from 'react-router-dom'
 import Add from './pages/Add/Add'
