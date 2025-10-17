d0105ade9222431fd7d3c00b427abfef39d91446
Hemanjali Saride <125305300+saridehemanjali@users.noreply.github.com>
2025-10-17 22:15:01 +0530

    Update API URL in assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from'./order_icon.png'
 import profile_image from'./profile_image.png'
 import upload_area from'./upload_area.png'
 import parcel_icon from'./parcel_icon.png'
-export const url="https://food-project-g7gq.onrender.com"
+export const url="https://food-project-backend-d8f9.onrender.com/"
 export const assets={
     logo,
     add_icon,
