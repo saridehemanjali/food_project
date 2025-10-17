5a1b927a9e3e4fe603b414cc5219f84f430afe98
Hemanjali Saride <125305300+saridehemanjali@users.noreply.github.com>
2025-10-17 20:14:43 +0530

    Update API URL to production endpoint

--- frontend/src/Content/StoreContent.jsx
+++ frontend/src/Content/StoreContent.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState({});
-    const url="http://localhost:4000"
+    const url="https://food-project-g7gq.onrender.com"
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
     const addToCart=async(itemId)=>{
