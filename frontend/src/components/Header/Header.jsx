26533fc24ad2894a823584d98d27774e08f88006
Hemanjali Saride <125305300+saridehemanjali@users.noreply.github.com>
2025-10-21 14:46:50 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Header.css'
 const Header = () => {
+    // Scroll smoothly to the menu section
+  const handleViewMenu = () => {
+    const menuSection = document.getElementById('explore-menu');
+    if (menuSection) {
+      menuSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <div className='header'>
         <div className='header-contents'>
             <h2>Order your Favourite food here</h2>
             <p> Choose from a diverse menu featuring a delectable array of dishes crafted </p>
-            <button> View Menu</button>
+            <button onClick={handleViewMenu}> View Menu</button>
         </div>
       
     </div>
